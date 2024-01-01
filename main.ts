4ff40dc851fcdae57af459f2bc5eb1c85102d0ff
a-iv <alexander.vl.ivanov@gmail.com>
2024-01-01 13:18:54 +0500

    Basic implementation.

--- main.ts
+++ main.ts
@@ -1,3 +1,233 @@
-basic.forever(function () {
-	
-})
+let CHRISTMAS_STRING_HEIGHT = 20
+let CHRISTMAS_STRING_WIDTH = 10
+let christmasString = neopixel.create(DigitalPin.P1, CHRISTMAS_STRING_WIDTH * CHRISTMAS_STRING_HEIGHT, NeoPixelMode.RGB)
+let BORDER_COLOR = neopixel.colors(NeoPixelColors.White)
+let BACKGROUND_COLOR = neopixel.colors(NeoPixelColors.Black)
+
+let FIGURE_MAPS = [
+    [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1]
+    ],
+    [
+        [1, 1],
+        [1, 1]
+    ],
+    [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 0],
+        [0, 1, 1]
+    ]
+]
+let FIGURE_COLORS = [
+    neopixel.colors(NeoPixelColors.Indigo),
+    neopixel.colors(NeoPixelColors.Blue),
+    neopixel.colors(NeoPixelColors.Orange),
+    neopixel.colors(NeoPixelColors.Yellow),
+    neopixel.colors(NeoPixelColors.Green),
+    neopixel.colors(NeoPixelColors.Violet),
+    neopixel.colors(NeoPixelColors.Red)
+]
+
+let currentField: number[][]
+let currentFigure: number[][]
+let currentX: number
+let currentY: number
+let nextFigure: number[][]
+let nextFigureIndex: number
+
+function getFieldWidth() {
+    return 1 + CHRISTMAS_STRING_WIDTH + 1
+}
+
+function getFieldHeight() {
+    return 1 + CHRISTMAS_STRING_HEIGHT + 1
+}
+
+function getEmptyRow(edgeColor: number, middleColor: number): number[] {
+    let result: number[] = []
+    for (let column = 0; column < getFieldWidth(); column++) {
+        if (column == 0 || column == getFieldWidth() - 1) {
+            result.push(edgeColor)
+        } else {
+            result.push(middleColor)
+        }
+    }
+    return result
+}
+
+function getEmptyField(): number[][] {
+    let result: number[][] = []
+    for (let row = 0; row < getFieldHeight(); row++) {
+        if (row == 0 || row == getFieldHeight() - 1) {
+            result.push(getEmptyRow(BACKGROUND_COLOR, BORDER_COLOR))
+        } else {
+            result.push(getEmptyRow(BORDER_COLOR, BACKGROUND_COLOR))
+        }
+    }
+    return result
+}
+
+function getFigureWidth(figure: number[][]): number {
+    return figure[0].length
+}
+
+function getFigureHeight(figure: number[][]): number {
+    return figure.length
+}
+
+function getColorFigure(map: number[][], color: number): number[][] {
+    let result: number[][] = []
+    let width = getFigureWidth(map)
+    let height = getFigureHeight(map)
+    let resultRow: number[];
+    for (let row = 0; row < height; row++) {
+        resultRow = []
+        for (let column = 0; column < width; column++) {
+            if (map[row][column]) {
+                resultRow.push(color)
+            } else {
+                resultRow.push(BACKGROUND_COLOR)
+            }
+        }
+        result.push(resultRow)
+    }
+    return result
+}
+
+function getFieldWithFigure(field: number[][], figure: number[][], x: number, y: number): number[][] {
+    let resultField: number[][] = []
+    let resultRow: number[];
+    for (let row = 0; row < getFieldHeight(); row++) {
+        if (row >= y && row < y + figure.length) {
+            resultRow = []
+            for (let column = 0; column < getFieldWidth(); column++) {
+                if (column >= x && column < x + figure[row - y].length && figure[row - y][column - x] != BACKGROUND_COLOR) {
+                    resultRow.push(figure[row - y][column - x])
+                } else {
+                    resultRow.push(field[row][column])
+                }
+            }
+        } else {
+            resultRow = field[row]
+        }
+        resultField.push(resultRow)
+    }
+    return resultField
+}
+
+function isFieldPossibleWithFigure(field: number[][], figure: number[][], x: number, y: number): boolean {
+    for (let row = 0; row < getFieldHeight(); row++) {
+        if (row >= y && row < y + figure.length) {
+            for (let column = 0; column < getFieldWidth(); column++) {
+                if (column >= x && column < x + figure[row - y].length &&
+                    figure[row - y][column - x] != BACKGROUND_COLOR &&
+                    field[row][column] != BACKGROUND_COLOR) {
+                    return false
+                }
+            }
+        }
+    }
+    return true
+}
+
+function generateNextFigureConfiguration() {
+    nextFigureIndex += 1
+    if (nextFigureIndex >= FIGURE_MAPS.length) {
+        nextFigureIndex = 0
+    }
+}
+
+function getNextFigure(): number[][] {
+    return getColorFigure(FIGURE_MAPS[nextFigureIndex], FIGURE_COLORS[nextFigureIndex])
+}
+
+function changeFigure(figure: number[][]) {
+    currentFigure = figure
+    currentX = Math.floor((getFieldWidth() - getFigureWidth(currentFigure) + 1) / 2)
+    currentY = 1
+    generateNextFigureConfiguration()
+    nextFigure = getNextFigure()
+}
+
+function endMovements() {
+    currentField = getFieldWithFigure(currentField, currentFigure, currentX, currentY)
+    changeFigure(nextFigure)
+}
+
+function showField(field: number[][]) {
+    for (let row = 0; row < CHRISTMAS_STRING_HEIGHT; row++) {
+        for (let column = 0; column < CHRISTMAS_STRING_WIDTH; column++) {
+            let shift: number
+            if (column % 2 == 0) {
+                shift = CHRISTMAS_STRING_HEIGHT - 1 - row
+            } else {
+                shift = row
+            }
+            christmasString.setPixelColor(shift + column * CHRISTMAS_STRING_HEIGHT, field[row + 1][column + 1])
+        }
+    }
+    christmasString.show()
+}
+
+function showFieldWithFigure() {
+    showField(getFieldWithFigure(currentField, currentFigure, currentX, currentY))
+}
+
+function moveLeft() {
+    if (isFieldPossibleWithFigure(currentField, currentFigure, currentX - 1, currentY)) {
+        currentX -= 1
+        showFieldWithFigure()
+    }
+}
+
+function moveRight() {
+    if (isFieldPossibleWithFigure(currentField, currentFigure, currentX + 1, currentY)) {
+        currentX += 1
+        showFieldWithFigure()
+    }
+}
+
+function moveDown() {
+    if (isFieldPossibleWithFigure(currentField, currentFigure, currentX, currentY + 1)) {
+        currentY += 1
+    } else {
+        endMovements()
+    }
+    showFieldWithFigure()
+}
+
+function startGame() {
+    currentField = getEmptyField()
+    generateNextFigureConfiguration()
+    changeFigure(getNextFigure())
+    showFieldWithFigure()
+}
+
+input.onButtonPressed(Button.A, moveLeft)
+input.onButtonPressed(Button.B, moveRight)
+input.onButtonPressed(Button.AB, moveDown)
+
+startGame()
--- test.ts
+++ test.ts
@@ -1,1 +1,227 @@
-// здесь помещаются тесты; они не будут скомпилированы, пока пакет используется в качестве расширения.
+let RUN_TESTS = true
+
+function getFigureJMap(): number[][] {
+    return [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0]
+    ]
+}
+
+function getFigureJColor(): number {
+    return 3
+}
+
+function getFigureJ(): number[][] {
+    return [
+        [0, 3, 0],
+        [0, 3, 0],
+        [3, 3, 0]
+    ]
+}
+
+function getFigureO(): number[][] {
+    return [
+        [4, 4],
+        [4, 4],
+    ]
+}
+
+function setUp() {
+    CHRISTMAS_STRING_WIDTH = 4
+    CHRISTMAS_STRING_HEIGHT = 7
+    BORDER_COLOR = 2
+    BACKGROUND_COLOR = 0
+    christmasString = neopixel.create(DigitalPin.P1, CHRISTMAS_STRING_WIDTH * CHRISTMAS_STRING_HEIGHT, NeoPixelMode.RGB)
+    FIGURE_MAPS = [getFigureJMap()]
+    FIGURE_COLORS = [getFigureJColor()]
+}
+
+function assertArrayEquals(array1: any[], array2: any[]) {
+    let string1 = JSON.stringify(array1)
+    let string2 = JSON.stringify(array2)
+    control.assert(string1 == string2, string1 + " != " + string2)
+}
+
+function testGetEmptyRow() {
+    assertArrayEquals(getEmptyRow(2, 0), [2, 0, 0, 0, 0, 2])
+}
+
+function testGetEmptyField() {
+    assertArrayEquals(getEmptyField(), [
+        [0, 2, 2, 2, 2, 0],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [0, 2, 2, 2, 2, 0],
+    ])
+}
+
+function testGetFigureWidth() {
+    let figure = [
+        [0, 0, 0],
+        [0, 0, 0],
+    ]
+    control.assert(getFigureWidth(figure) == 3)
+    control.assert(getFigureHeight(figure) == 2)
+}
+
+function testGetColorFigure() {
+    assertArrayEquals(getColorFigure(getFigureJMap(), getFigureJColor()), getFigureJ())
+}
+
+function testGetFieldWithFigure() {
+    assertArrayEquals(getFieldWithFigure(getEmptyField(), getFigureJ(), 1, 1), [
+        [0, 2, 2, 2, 2, 0],
+        [2, 0, 3, 0, 0, 2],
+        [2, 0, 3, 0, 0, 2],
+        [2, 3, 3, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [0, 2, 2, 2, 2, 0],
+    ])
+}
+
+function testIsFieldPossibleWithFigure() {
+    control.assert(!isFieldPossibleWithFigure(getEmptyField(), getFigureJ(), 0, 1), "Left border")
+    control.assert(isFieldPossibleWithFigure(getEmptyField(), getFigureJ(), 1, 1))
+    control.assert(isFieldPossibleWithFigure(getEmptyField(), getFigureJ(), 3, 1))
+    control.assert(!isFieldPossibleWithFigure(getEmptyField(), getFigureJ(), 4, 1), "Right border")
+    control.assert(isFieldPossibleWithFigure(getEmptyField(), getFigureJ(), 1, 5))
+    control.assert(!isFieldPossibleWithFigure(getEmptyField(), getFigureJ(), 1, 6), "Bottom border")
+}
+
+function testGenerateNextFigureConfiguration() {
+    nextFigureIndex = 0
+    generateNextFigureConfiguration()
+    control.assert(nextFigureIndex >= 0 && nextFigureIndex < FIGURE_MAPS.length)
+}
+
+function testGetNextFigure() {
+    nextFigureIndex = 0
+    assertArrayEquals(getNextFigure(), getFigureJ())
+}
+
+function setUpGameTest(field: number[][], figure: number[][], x: number, y: number, next: number[][]) {
+    currentField = field
+    currentFigure = figure
+    currentX = x
+    currentY = y
+    nextFigure = next
+}
+
+function assertChangeFigure(figure: number[][], resultX: number, resultY: number) {
+    setUpGameTest(getEmptyField(), [], -1, -1, [])
+    changeFigure(figure)
+    assertArrayEquals(currentFigure, figure)
+    control.assert(currentX == resultX)
+    control.assert(currentY == resultY)
+    control.assert(nextFigure.length > 0)
+}
+
+function testChangeFigure() {
+    assertChangeFigure([
+        [0, 3, 0],
+        [0, 3, 0],
+        [3, 3, 0]
+    ], 2, 1)
+    assertChangeFigure([
+        [4, 4],
+        [4, 4],
+    ], 2, 1)
+    assertChangeFigure([
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [5, 5, 5, 5],
+        [0, 0, 0, 0],
+    ], 1, 1)
+}
+
+function testEndMovements() {
+    setUpGameTest(getEmptyField(), getFigureJ(), 1, 5, getFigureO())
+    endMovements()
+    assertArrayEquals(currentField, [
+        [0, 2, 2, 2, 2, 0],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 2],
+        [2, 0, 3, 0, 0, 2],
+        [2, 0, 3, 0, 0, 2],
+        [2, 3, 3, 0, 0, 2],
+        [0, 2, 2, 2, 2, 0],
+    ])
+    assertArrayEquals(currentFigure, getFigureO())
+}
+
+function testShowField() {
+    showField(getEmptyField())
+}
+
+function testShowFieldWithFigure() {
+    setUpGameTest(getEmptyField(), getFigureJ(), 1, 1, getFigureO())
+    showFieldWithFigure()
+}
+
+function testMoveLeft() {
+    setUpGameTest(getEmptyField(), getFigureJ(), 2, 1, getFigureO());
+    moveLeft()
+    control.assert(currentX == 1)
+
+    setUpGameTest(getEmptyField(), getFigureJ(), 1, 1, getFigureO());
+    moveLeft()
+    control.assert(currentX == 1, "Left border")
+}
+
+function testMoveRight() {
+    setUpGameTest(getEmptyField(), getFigureJ(), 2, 1, getFigureO());
+    moveRight()
+    control.assert(currentX == 3)
+
+    setUpGameTest(getEmptyField(), getFigureJ(), 3, 1, getFigureO());
+    moveRight()
+    control.assert(currentX == 3, "Right border")
+}
+
+function testMoveDown() {
+    setUpGameTest(getEmptyField(), getFigureJ(), 2, 4, getFigureO())
+    moveDown()
+    control.assert(currentY == 5)
+    assertArrayEquals(currentFigure, getFigureJ())
+
+    setUpGameTest(getEmptyField(), getFigureJ(), 2, 5, getFigureO())
+    moveDown()
+    control.assert(currentY == 1)
+    assertArrayEquals(currentFigure, getFigureO())
+}
+
+function testStartGame() {
+    startGame()
+}
+
+if (RUN_TESTS) {
+    setUp()
+    testGetEmptyRow()
+    testGetEmptyField()
+    testGetFigureWidth()
+    testGetColorFigure()
+    testGetFieldWithFigure()
+    testIsFieldPossibleWithFigure()
+    testGenerateNextFigureConfiguration()
+    testGetNextFigure()
+    testChangeFigure()
+    testEndMovements()
+    testShowField()
+    testShowFieldWithFigure()
+    testMoveLeft()
+    testMoveRight()
+    testMoveDown()
+    testStartGame()
+    console.log("PASSED")
+}
